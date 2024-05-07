3abcb206920e59739bae29f9ae5e3809bfae99b5
Josh Leslie <30437767+JoshMLeslie@users.noreply.github.com>
2024-05-06 23:54:21 -0400

    fix canada route. cleanup data

--- zones/north-east.js
+++ zones/north-east.js
@@ -1,23 +1,28 @@
 const NE = [
 	// Woodstock, New Brunswick - Sudbury, ON
 	[
-		{state: 'New Brunswick', city: 'Woodstock', country: 'Canada'},
+		{city: 'Woodstock', state: 'New Brunswick', country: 'Canada'},
 		{city: 'Houlton', state: 'Maine', country: 'USA'},
 		{city: 'Jackman', state: 'Maine', country: 'USA'},
-		{state: 'Quebec', city: 'Montreal', country: 'Canada'},
+		{city: 'Montreal', state: 'Quebec', country: 'Canada'},
+		{city: 'Ottawa', state: 'Ontario', country: 'Canada'},
+		{city: 'Sudbury', state: 'Ontario', country: 'Canada'},
+	],
+	// Ottawa - Toronto Jumper
+	[
 		{city: 'Ottawa', state: 'Ontario', country: 'Canada'},
 		{city: 'Toronto', state: 'Ontario', country: 'Canada'},
 	],
 	// Woodstock, New Brunswick - Stamford, CT
 	[
-		{state: 'New Brunswick', city: 'Woodstock', country: 'Canada'},
+		{city: 'Woodstock', state: 'New Brunswick', country: 'Canada'},
 		{city: 'Houlton', state: 'Maine', country: 'USA'},
 		{city: 'Bangor', state: 'Maine', country: 'USA'},
 		{city: 'Portland', state: 'Maine', country: 'USA'},
-		{state: 'Massachusetts', city: 'Boston', country: 'USA'},
-		{state: 'Rhode Island', city: 'Providence', country: 'USA'},
-		{state: 'Connecticut', city: 'Hartford', country: 'USA'},
-		{state: 'Connecticut', city: 'New Haven', country: 'USA'},
+		{city: 'Boston', state: 'Massachusetts', country: 'USA'},
+		{city: 'Providence', state: 'Rhode Island', country: 'USA'},
+		{city: 'Hartford', state: 'Connecticut', country: 'USA'},
+		{city: 'New Haven', state: 'Connecticut', country: 'USA'},
 		{city: 'Bridgeport', state: 'Connecticut', country: 'USA'},
 		{city: 'Stamford', state: 'Connecticut', country: 'USA'},
 	],
@@ -26,7 +31,7 @@ const NE = [
 		{city: 'Boston', state: 'Massachusetts', country: 'USA'},
 		{city: 'Worcester', state: 'Massachusetts', country: 'USA'},
 		{city: 'Springfield', state: 'Massachusetts', country: 'USA'},
-		{state: 'New York', city: 'Albany', country: 'USA'},
+		{city: 'Albany', state: 'New York', country: 'USA'},
 		{city: 'Syracuse', state: 'New York', country: 'USA'},
 		{city: 'Rochester', state: 'New York', country: 'USA'},
 		{city: 'Buffalo', state: 'New York', country: 'USA'},
@@ -34,13 +39,13 @@ const NE = [
 	// CT - MA Jumper
 	[
 		{city: 'Springfield', state: 'Massachusetts', country: 'USA'},
-		{state: 'Connecticut', city: 'Hartford', country: 'USA'},
+		{city: 'Hartford', state: 'Connecticut', country: 'USA'},
 	],
 	// Montreal - New York
 	[
-		{state: 'Quebec', city: 'Montreal', country: 'Canada'},
-		{state: 'Vermont', city: 'Burlington', country: 'USA'},
-		{state: 'New York', city: 'Albany', country: 'USA'},
+		{city: 'Montreal', state: 'Quebec', country: 'Canada'},
+		{city: 'Burlington', state: 'Vermont', country: 'USA'},
+		{city: 'Albany', state: 'New York', country: 'USA'},
 		{city: 'NYC', state: 'New York', country: 'USA'},
 	],
 	[
