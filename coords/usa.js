84b1b3693259e475c4bfb5dc958abeb95d14b873
Josh Leslie <30437767+JoshMLeslie@users.noreply.github.com>
2024-05-20 13:06:46 -0400

    Move USA coords to dir

---
+++ coords/usa.js
@@ -0,0 +1,104 @@
+export default {
+		"Alaska": {
+			"Anchorage": [61.2181, -149.9003]
+		},
+		"California": {
+			"Los Angeles": [34.0522, -118.2437],
+			"Oakland": [37.8044, -122.2711],
+			"Sacramento": [38.5816, -121.4944],
+			"San Diego": [32.7157, -117.1611],
+			"San Francisco": [37.7749, -122.4194],
+			"San Jose": [37.3382, -121.8863]
+		},
+		"Connecticut": {
+			"Bridgeport": [41.1865, -73.1952],
+			"Hartford": [41.7658, -72.6734],
+			"New Haven": [41.3083, -72.9279],
+			"Stamford": [41.0534, -73.5387],
+			"Waterbury": [41.5581, -73.0515]
+		},
+		"Delaware": {
+			"Dover": [39.1582, -75.5244],
+			"Newark": [39.6837, -75.7497],
+			"Wilmington": [39.7391, -75.5398]
+		},
+		"Maine": {
+			"Bangor": [44.8014, -68.7778],
+			"Houlton": [46.1252, -67.8399],
+			"Jackman": [45.6298, -70.2536],
+			"Portland": [43.6591, -70.2568],
+			"Portsmouth": [43.0651, -70.7796]
+		},
+		"Maryland": {
+			"Annapolis": [38.9784, -76.4922],
+			"Baltimore": [39.2904, -76.6122],
+			"Frederick": [39.4142, -77.4105],
+			"Gaithersburg": [39.1434, -77.2014],
+			"Rockville": [39.0834, -77.1553]
+		},
+		"Massachusetts": {
+			"Boston": [42.3601, -71.0589],
+			"Cambridge": [42.3736, -71.1097],
+			"Lowell": [42.6334, -71.3162],
+			"Springfield": [42.1015, -72.5898],
+			"Worcester": [42.2626, -71.8023]
+		},
+    "Michigan": {
+			"Gaylord": [45.0276, -84.6741],
+      "Lansing": [42.7325, -84.5555],
+      "Mackinaw City": [45.7833, -84.7333],
+      "Mt. Pleasant": [43.5978, -84.7675],
+      "Petoskey": [45.3736, -84.9553],
+			"Sault Ste. Marie": [46.4913, -84.3666]
+    },
+		"New Jersey": {
+			"Atlantic City": [39.3643, -74.4229],
+			"Camden": [39.9259, -75.1196],
+			"Jersey City": [40.7282, -74.0776],
+			"Newark": [40.7357, -74.1724],
+			"Paterson": [40.9168, -74.1718],
+			"Trenton": [40.2206, -74.7597]
+		},
+		"New York": {
+			"Albany": [42.6526, -73.7562],
+			"Buffalo": [42.8864, -78.8784],
+			"NYC": [40.7128, -74.006],
+			"Rochester": [43.1566, -77.6088],
+			"Syracuse": [43.0481, -76.1474]
+		},
+		"Oregon": {
+			"Eugene": [44.0521, -123.0868],
+			"Portland": [45.5051, -122.675]
+		},
+		"Pennsylvania": {
+			"Burnham": [40.6366, -77.5675],
+			"Erie": [42.1292, -80.0851],
+			"Harrisburg": [40.2732, -76.8867],
+			"Philadelphia": [39.9526, -75.1652],
+			"Pittsburgh": [40.4406, -79.9959],
+			"Scranton": [41.408, -75.6624]
+		},
+		"Rhode Island": {
+			"Providence": [41.824, -71.4128]
+		},
+		"Vermont": {
+			"Burlington": [44.4759, -73.2121]
+		},
+		"Virginia": {
+			"Alexandria": [38.8048, -77.0469],
+			"Arlington": [38.8816, -77.091],
+			"Newport News": [37.0871, -76.473],
+			"Norfolk": [36.8508, -76.2859],
+			"Richmond": [37.5407, -77.436],
+			"Roanoke": [37.2707, -79.9414],
+			"Virginia Beach": [36.8529, -75.978]
+		},
+		"Washington": {
+			"Olympia": [47.0379, -122.9007],
+			"Seattle": [47.6062, -122.3321],
+			"Tacoma": [47.2529, -122.4443]
+		},
+		"West Virginia": {
+			"Charleston": [38.3498, -81.6326]
+		}
+	}
