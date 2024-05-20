1a5a2fec9f2ca88ef7cfd8738d4746bcfd9fb6cb
Josh Leslie <30437767+JoshMLeslie@users.noreply.github.com>
2024-05-20 13:02:38 -0400

    Update coords.js to barrel ish file

--- coords.js
+++ coords.js
@@ -1,33 +1,6 @@
 export default {
-	"Canada": {
-		"British Columbia": {
-			"Prince George": [53.9171, -122.7494],
-			"Vancouver": [49.2827, -123.1207],
-			"Victoria": [48.4284, -123.3656]
-		},
-		"New Brunswick": {
-			"Woodstock": [46.1514, -67.5998]
-		},
-		"Ontario": {
-			"Ottawa": [45.4215, -75.691],
-			"Sudbury": [46.5227, -80.9524],
-			"Toronto": [43.65107, -79.347015],
-			"Sault Ste. Marie": [46.5251, -84.3421]
-		},
-		"Quebec": {
-			"Montreal": [45.5017, -73.5673],
-			"Quebec City": [46.8139, -71.208],
-			"Saguenay": [48.3928, -71.1040]
-		},
-		"Yukon": {
-			"Whitehorse": [60.7212, -135.0568]
-		}
-	},
-	"Mexico": {
-		"Baja California": {
-			"Tijuana": [32.5149, -117.0382]
-		}
-	},
+	"Canada": import "./coords/canada.js", 
+	"Mexico": import "./coords/mexico.js", 
 	"USA": {
 		"Alaska": {
 			"Anchorage": [61.2181, -149.9003]
