e52e558c7ab91e6630722a05781dfe90672d4dbc
Josh Leslie <hello@joshmleslie.com>
2024-06-17 14:54:34 -0400

    update central states file for future routes

--- assets/js/zones/central.js
+++ assets/js/zones/central.js
@@ -1,32 +1,4 @@
-const MIDWEST_STATES = {
-	Illinois: {
-		cities: ['Chicago', 'Springfield', 'Peoria'],
-		routes: [
-			['Chicago', 'Springfield'],
-			['Springfield', 'Peoria'],
-			['Chicago', 'Peoria'],
-		],
-		phases: [['Chicago', 'Springfield', 'Peoria']],
-	},
-	Indiana: {
-		cities: ['Indianapolis', 'Fort Wayne', 'Evansville'],
-		routes: [
-			['Indianapolis', 'Fort Wayne'],
-			['Fort Wayne', 'Evansville'],
-			['Indianapolis', 'Evansville'],
-		],
-		phases: [['Indianapolis', 'Fort Wayne', 'Evansville']],
-	},
-	Iowa: {
-		cities: ['Des Moines', 'Cedar Rapids', 'Davenport'],
-		routes: [
-			['Des Moines', 'Cedar Rapids'],
-			['Cedar Rapids', 'Davenport'],
-			['Des Moines', 'Davenport'],
-		],
-		phases: [['Des Moines', 'Cedar Rapids', 'Davenport']],
-	},
-	// Add more states as needed...
-};
+const CENTRAL_STATES = [
 
-export default MIDWEST_STATES;
+];
+export default CENTRAL_STATES;
